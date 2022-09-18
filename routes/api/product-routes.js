5c3268be2448e52958ccccc179510b7f89cadb5a
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-09-18 11:20:50 -0600

    all models written

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,38 @@
 const router = require('express').Router();
+const { json } = require('sequelize/types');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+    // be sure to include its associated Category and Tag data
+  try {
+   const allProductData = await Product.findAll({
+     include: [{ model: Category, model: Tag}],
+    });
+    res.status(200).json(allProductData)
+  } catch (err) {
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+router.get('/:id', async (req, res) => {
+   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const oneProductData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, model: Tag}],
+  });
+    if(!oneProductData) {
+      res.status(404).json({ message: "No product with that id" });
+      return;
+    }
+    res.status(200).json(oneProductData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
